98162605b38845c93d02a06d3d1222d3da750493
minhtris123 <81600564+minhtris123@users.noreply.github.com>
2021-06-21 18:41:09 +0700

    Add files via upload

---
+++ DBNhaCungCap.swift
@@ -0,0 +1,49 @@
+//
+//  DBNhaCungCap.swift
+//  Quan Ly Ban Hang Vat Lieu Xay
+//
+//  Created by MacOS on 5/29/21.
+//  Copyright © 2021 DoAnIOS. All rights reserved.
+//
+
+import Firebase
+
+class DBNhaCungCap {
+    var db = Firestore.firestore().collection("NhaCungCap")
+    
+    func addNCC(ncc: NhaCungCap, completion: @escaping (Error?) -> ()) {
+        let id = db.document().documentID
+        ncc.id = id
+        
+        db.document(id).setData(ncc.getData()){
+            data in
+            completion(data)
+        }
+    }
+    
+    func delNCCByID(id: String, completion: @escaping (Error?) -> ()) {
+        db.document(id).delete(){
+            error in
+            completion(error)
+        }
+    }
+    
+    func editNCC(ncc: NhaCungCap, completion: @escaping (Error?) -> ()) {
+        db.document(ncc.id).setData(ncc.getData()){
+            data in
+            completion(data)
+        }
+    }
+    
+    func getAll(completion: @escaping ([NhaCungCap]?) -> ()) {
+        db.getDocuments { (snap, error) in
+            if let snap = snap?.documents, error == nil {
+                var arr = [NhaCungCap]()
+                for item in snap {
+                    arr.append(NhaCungCap(data: item.data()))
+                }
+                completion(arr)
+            }
+        }
+    }
+}
