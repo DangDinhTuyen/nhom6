59d5e4d310d1dbb89d7dc926d54d6f4b87804b32
nguyenluan21 <84944935+nguyenluan21@users.noreply.github.com>
2021-06-23 13:08:34 +0700

    Add files via upload

---
+++ DBNhanVien.swift
@@ -0,0 +1,35 @@
+//
+//  DBNhanVien.swift
+//  Quan Ly Ban Hang Vat Lieu Xay
+//
+//  Created by MacOS on 5/29/21.
+//  Copyright © 2021 DoAnIOS. All rights reserved.
+//
+
+import Firebase
+
+class DBNhanVien {
+    var db = Firestore.firestore().collection("NhanVien");
+    
+    func addNhanVien(nhanVien: NhanVien, completion: @escaping (Error?)->()) {
+        db.document(nhanVien.id).setData(nhanVien.getData()) { (error) in
+            completion(error)
+        }
+    }
+    
+    func getNhanVienByID(id: String, completion: @escaping (NhanVien?)->()) {
+        db.document(id).getDocument { (document, error) in
+            if let data = document?.data() {
+                completion(NhanVien(data: data))
+            } else {
+                completion(nil)
+            }
+        }
+    }
+    
+    func delNhanVien(id: String, completion: @escaping (Error?)->()) {
+        db.document(id).delete() { (error) in
+            completion(error)
+        }
+    }
+}
